97e11d86fdc67a16cf18d738b6a7b166ca2efa26
Brianee <bnguerrero@cpp.edu>
2023-05-18 00:38:03 -0700

    edit WeatherAPI

--- StrategyandObserver/WeatherAPI.java
+++ StrategyandObserver/WeatherAPI.java
@@ -1,11 +1,32 @@
 package CS5800Final.StrategyandObserver;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.tools.JavaFileObject;
 
 public class WeatherAPI 
 {
-    WeatherData weatherData;
+    private String URL = "https://api.weatherapi.com";
+    private String API_KEY = "14b115ece73347d4a0871208231805";
     
-    public WeatherData getWeatherData()
+    public WeatherData getWeatherData(String location)
     {
-        return weatherData;
+        String endpoint = URL + "/v1/current.json?key=" + API_KEY + "&q=" + location;
+        
+        try 
+        {
+            URL url = new URL(endpoint);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+           
+        } 
+        catch(IOException e) 
+        {
+            
+        }
     }
 }
